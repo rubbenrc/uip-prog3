c330b891abb2262255e4ea5835055617e7664575
rubbenrc <rubenabrego13@hotmail.com>
2015-11-14 23:53:26 -0600

    quiz #1

---
+++ tarea/quiz#1 ruben abrego.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        perimetro y area de un rectangulo
+
+# Author:      rubben rc
+# Copyright:   (c) programar 2015
+
+#-------------------------------------------------------------------------------
+
+print"quiz#1"
+base=5
+altura=7
+area=(int(base)*(altura))
+perimetro=(2*int(base))+(2*int(altura))
+
+print"el area de un rectangulo es:",area
+print"el perimetro de un rectangulo es:",perimetro
