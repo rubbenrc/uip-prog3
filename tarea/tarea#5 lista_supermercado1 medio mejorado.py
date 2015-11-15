618687098419dd474b5aa8d0a1969e0fbf5dfcf4
rubbenrc <rubenabrego13@hotmail.com>
2015-11-14 23:23:48 -0600

    mejoras

---
+++ tarea/tarea#5 lista_supermercado1 medio mejorado.py
@@ -0,0 +1,155 @@
+#-------------------------------------------------------------------------------
+# Name:        Tarea#5 lista de supermercado de la empresa ABC
+# Autor:      programar
+#
+# Creado:     30/10/2015
+# Copyright:   (c) programar 2015
+# Licence:     <your licence 1.2>
+
+#Usted fue contratado por la startup ABC como programador.
+#Ellos le solicitan que programe la primera fase de un mo?dulo que represente
+#una lista de supermercados.
+#El programa debe presentar un menu? donde pueda elegir, por lo poco, si quiero
+#agregar, eliminar y ver los elementos de mi lista de supermercado.
+#Se pueden manejar hasta 3 listas de supermercado.
+#ABC esta? interesada en funciones, manejo de excepciones y m?dulos.
+# Este c?digo debe servir para posteriores proyectos.
+#-------------------------------------------------------------------------------
+
+saludo="Hola Bienvenido a la Empresa ABC\n".capitalize()
+print saludo.center(50," ")
+entrada= "Programa para agregar,eliminar y ver la lista de supermercado\n"
+print entrada.upper()
+#------------------------------------------------------------------------------------------------------------
+#menu agregar
+import msvcrt
+lista_supermercado_funciones=0
+lista_supermercado_manejo_excepciones=0
+lista_supermercado_modulos=0
+lista_supermercado_funciones=[]
+lista_supermercado_manejo_excepciones=[]
+lista_supermercado_modulos=[]
+
+print 'elija la opcion de su gusto \n'
+print '1) agregar lista de supermercado funciones '
+print '2) agregar lista de supermercado excepciones'
+print '3) agregar lista de supermercado modulos'
+print "4) ver lista de supermercados"
+print "5) eliminar"
+
+opcion = int(raw_input('elija opcion: '))
+
+if opcion == 1:
+
+        print("\nlista de supermercado funciones")
+        lista_supermercado_funciones= [ (raw_input("Ingrese articulos a comprar: "))]
+        print lista_supermercado_funciones
+
+        lista_supermercado_funciones.append(raw_input("agregar otro elemento a la lista de supermecado: "))
+        print lista_supermercado_funciones
+
+elif opcion == 2:
+
+       print ("\nlista de manejo de excepciones")
+       lista_supermercado_manejo_excepciones= [(raw_input("Ingrese articulos a comprar: "))]
+       print lista_supermercado_manejo_excepciones
+
+       lista_supermercado_manejo_excepciones.append(raw_input("agregar otro elementos a la lista de supermecado:  "))
+       print lista_supermercado_manejo_excepciones
+
+
+elif opcion == 3:
+       print("\nlistas de modulos")
+       lista_modulos=[raw_input("Ingrese articulos a comprar: ")]
+       print lista_modulos
+
+       lista_modulos.append(raw_input("agregar otro elementos a la lista de supermecado:\n"  ))
+       print lista_modulos
+else:
+    print("opcion invalida")
+
+#-------------------------------------------------------------------------------------------------------------
+#ver lista
+x=lista_supermercado_funciones
+x=lista_supermercado_manejo_excepciones
+x=lista_supermercado_modulos
+
+print("\nver lista")
+if opcion == 4:
+
+    print("Diga  a que lista quiesiera ver los articulos agregados ")
+print '1) lista de supermercado funciones '
+print '2) lista de supermercado excepciones'
+print '3) lista de supermercado modulos'
+opcion = int(raw_input('elija opcion: '))
+
+if opcion == 1:
+
+    lista_supermercado_funciones=[  ]
+    for x in lista_supermercado_funciones:
+     print x
+     break
+
+elif opcion == 2:
+    lista_supermercado_manejo_excepciones=[ ]
+    for x in lista_supermercado_manejo_excepciones:
+     print x
+     break
+elif opcion == 3:
+
+    lista_supermercado_modulos=[  ]
+    for x in lista_supermercado_modulos:
+     print x
+     break
+else:
+    print "articulo no encontrado"
+    print("lista de supermercado funciones:",lista_supermercado_funciones)
+    print ("lista de supermercao manejo de excepciones",lista_supermercado_excepciones)
+    print("lista de supermercado modulos",lista_supermercado_modulos)
+#-----------------------------------------------------------------------------------------------------------
+#opcion 5 eliminar
+lista_supermercado_funciones=[]
+lista_supermercado_manejo_excepciones=[]
+lista_supermercado_modulos=[]
+
+print("\n eliminar")
+if opcion == 5:
+
+    print("Diga  a que lista quiere eliminarle articulos\n")
+print ') lista de supermercado funciones '
+print '2) lista de supermercado excepciones'
+print '3) lista de supermercado modulos'
+opcion = int(raw_input('elija opcion: '))
+
+if opcion == 1:
+
+    lista_supermercado_funciones=(raw_input("? Diga cual articulo quiere eliminar:"))
+    print lista_supermercado_funciones
+
+    lista_supermercado_funciones.remove(raw_input("? Diga cual otro articulo quiere eliminar:"))
+    print lista_supermercado_funciones
+
+elif opcion == 2:
+    lista_supermercado_manejo_excepciones= (raw_input("? Diga cual articulo quiere eliminar:"))
+    print lista_supermercado_manejo_excepciones
+
+    lista_supermercado_manejo_excepciones.remove (raw_input("? Diga cual articulo quiere eliminar:"))
+    print lista_supermercado_manejo_excepciones
+
+elif opcion == 3:
+    lista_supermercado_modulos=(raw_input("? Diga cual articulo quiere eliminar:"))
+    print lista_supermercado_modulos
+
+    lista_supermercado_modulos.remove (raw_input("? Diga cual articulo quiere eliminar:"))
+    print lista_supermercado_modulos
+
+
+
+else:
+    print "articulo no encontrado"
+
+
+msvcrt.getch()
+#--------------------------------------------------------------------------------------------------------------------------------------
+#profe no se como almacenar el dato introducido y como hacer ver la lista
+
---
+++ tarea/tarea#6 encuesta mejorada.py
@@ -0,0 +1,131 @@
+#-------------------------------------------------------------------------------
+# Name:        tarea#6 encuesta
+#
+# Author:      programar
+#
+# Created:     30/10/2015
+# Copyright:   (c) programar 2015
+# Licence:     <your licence>
+
+#Elaborar un programa en Python que encueste a 10 personas y las clasifique
+#seg?n el deporte que practica.
+#La lista de deportes v?lidos son: Ajedrez,Atletismo, Baloncesto, F?tbol, Karate
+#, Nataci?n, Volleyball, Flag y Ping Pong.
+
+#Puede darse el caso que no le guste ninguno de los deportes anteriores.
+#Si es as?, entonces se puede seleccionar la opci?n Otros.
+#Al final el programa debe mostrar estad?sticas de la encuesta e indicar
+#cu?l es el deporte con mayor
+# preferencia de los encuestados.
+#-------------------------------------------------------------------------------
+import msvcrt
+#inicio
+titulo="Encuesta Deportes\n".capitalize()
+print titulo.center(50," ")
+entrada= "Clasificacion de deportes que practican las personas\n"
+print entrada.upper()
+#-------------------------------------------------------------------------------
+#pregunta
+pregunta=0
+c1=1
+ajedrez=0
+atletismo=0
+futbol=0
+baloncesto=0
+karate=0
+natacion=0
+volleyball=0
+flag=0
+pingpong=0
+otros=0
+
+while pregunta >1 or pregunta <=10 :
+    pregunta=pregunta+1
+    print ("\n?Que deporte usted practica de la siguiente lista?\n ")
+    print"1) Ajedrez"
+    print'2) Atletismo'
+    print'3) Baloncesto '
+    print"4) Futbol"
+    print"5) Karate "
+    print"6) Natacion"
+    print"7) Volleyball"
+    print"8) Flag"
+    print"9) Ping Pong"
+    print"10) otros"
+    opcion = int(raw_input('elija opcion: '))
+
+    if opcion == 1:
+
+        ajedrez=ajedrez+1
+        print "\najedrez"
+
+    elif opcion == 2:
+
+        atletismo=atletismo+1
+        print "\natletismo"
+
+    elif opcion == 3:
+
+        baloncesto=baloncesto+1
+        print "\nbaloncesto"
+
+
+
+    elif opcion == 4:
+
+        futbol=futbol+1
+        print "\nfutbol"
+
+    elif opcion == 5:
+
+        karate=karate+1
+        print "\nkarate"
+
+
+    elif  opcion == 6:
+
+        natacion=natacion+1
+        print "\nnatacion"
+
+
+    elif  opcion == 7:
+
+        volleyball=volleyball+1
+        print "\nvolleybol"
+
+
+    elif  opcion == 8:
+
+        flag=flag+1
+        print "\nflag"
+
+    elif  opcion == 9:
+
+        pingpong=pingpong+1
+        print "\nping pong"
+
+    elif opcion==10:
+
+        otros=otros+1
+        print"\notros"
+    else:
+        print"\nelija opciones establecidas"
+
+    if pregunta >=10:
+        break
+
+
+
+print("\nEl total de encuestado de los diferentes deportes son:\n")
+print("ajedrez",ajedrez)
+print("atletismo:",atletismo)
+print("baloncesto:",baloncesto)
+print("futbol:",futbol)
+print("karate:",karate)
+print("natacion:",natacion)
+print("volleyball:",volleyball)
+print("flag:",flag)
+print("pingpong",pingpong)
+print("otros",otros)
+msvcrt.getch()
+
